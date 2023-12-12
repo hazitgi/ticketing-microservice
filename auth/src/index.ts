befddd636929f2c027025a6d69013aed78df04a2
hazi-tgi <mohamedhaseeb@ateamsoftsolutions.com>
2023-12-12 18:37:38 +0530

    feat: auth service done

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,10 +1,14 @@
-import express from 'express';
-import { json } from 'body-parser';
+import express from "express";
+import "express-async-errors";
+import { json } from "body-parser";
+import mongoose from "mongoose";
 
-import { currentUserRouter } from './routes/current-user';
-import { signinRouter } from './routes/signin';
-import { signoutRouter } from './routes/signout';
-import { signupRouter } from './routes/signup';
+import { currentUserRouter } from "./routes/current-user";
+import { signinRouter } from "./routes/signin";
+import { signoutRouter } from "./routes/signout";
+import { signupRouter } from "./routes/signup";
+import { errorHandler } from "./middlewares/error-handler";
+import { NotFoundError } from "./errors/not-found-error";
 
 const app = express();
 app.use(json());
@@ -14,6 +18,35 @@ app.use(signinRouter);
 app.use(signoutRouter);
 app.use(signupRouter);
 
-app.listen(3000, () => {
-  console.log('Listening on port 3000!!!!!!!!');
+app.all("*", async (req, res) => {
+  throw new NotFoundError();
 });
+
+app.use(errorHandler);
+
+const start = async () => {
+  try {
+    // Replace 'your-database-name' with your actual database name
+    const dbName = "ticketing";
+    const mongoHost = "auth-mongo-db-clusterip-srv"; // This should match the service name in your Kubernetes cluster
+
+    const dbUsername = process.env.MONGO_INITDB_ROOT_USERNAME;
+    const dbPassword = process.env.MONGO_INITDB_ROOT_PASSWORD;
+    // const connectionString = `mongodb://${dbUsername}:${dbPassword}@${mongoHost}:27017/${dbName}`;
+    const connectionString = `mongodb://${mongoHost}:27017/`;
+
+    console.log({connectionString});
+    
+    await mongoose.connect(connectionString);
+
+    console.log("Connected to MongoDb");
+  } catch (err) {
+    console.error(err);
+  }
+
+  app.listen(3000, () => {
+    console.log("Listening on port 3000!!!!!!!!");
+  });
+};
+
+start();
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,30 +1,41 @@
-import express, { Request, Response } from "express";
-import { body, validationResult } from "express-validator";
+import express, { Request, Response } from 'express';
+import { body, validationResult } from 'express-validator';
+import { User } from '../models/user';
+import { RequestValidationError } from '../errors/request-validation-error';
+import { BadRequestError } from '../errors/bad-request-error';
 
 const router = express.Router();
 
 router.post(
-  "/api/users/signup",
+  '/api/users/signup',
   [
-    body("email").isEmail().withMessage("Email must be valid"),
-    body("password")
+    body('email')
+      .isEmail()
+      .withMessage('Email must be valid'),
+    body('password')
       .trim()
       .isLength({ min: 4, max: 20 })
-      .withMessage("Password must be between 4 and 20 characters"),
+      .withMessage('Password must be between 4 and 20 characters')
   ],
-  (req: Request, res: Response) => {
+  async (req: Request, res: Response) => {
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
-      return res.status(400).send(errors.array());
+      throw new RequestValidationError(errors.array());
     }
+
     const { email, password } = req.body;
 
-    console.log("Creating a user...");
+    const existingUser = await User.findOne({ email });
 
-    res.send({});
+    if (existingUser) {
+      throw new BadRequestError('Email in use');
+    }
 
-    // new User({ email, password })
+    const user = User.build({ email, password });
+    await user.save();
+
+    res.status(201).send(user);
   }
 );
 
