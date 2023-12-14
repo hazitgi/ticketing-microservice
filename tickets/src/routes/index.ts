f1b29e245db408fb6236c980d50145286df7cfda
hazi-tgi <mohamedhaseeb@ateamsoftsolutions.com>
2023-12-14 19:09:31 +0530

    feat: ticket service

--- tickets/src/routes/index.ts
+++ tickets/src/routes/index.ts
@@ -4,6 +4,7 @@ import { Ticket } from '../models/ticket';
 const router = express.Router();
 
 router.get('/api/tickets', async (req: Request, res: Response) => {
+  console.log("finding tickets");
   const tickets = await Ticket.find({});
 
   res.send(tickets);
